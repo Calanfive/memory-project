7b70a509f2f25accc4fb508733c2b431933302ca
calanfive <camille.lelan@gmail.com>
2023-08-16 15:58:29 +0200

    colors tiles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
+const btnStart = document.querySelector("#start_button") as HTMLButtonElement;
 
 let cpt = 0;
 
@@ -9,5 +9,16 @@ btnStart.addEventListener("click", () => {
 function init(){
     console.log('start')
     btnStart.remove();
-}
+    const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
+    const tiles = new Array(16).fill('').map( (_, i) => {
+        const tile = document.createElement("div")
+        tile.setAttribute("class", "tile")
+        tile.style.width = "50px"
+        tile.style.height = "50px"
+        tile.style.backgroundColor = colors[Math.floor(i/2)]
+        return tile
+    })
+    const body = document.querySelector("body") as HTMLBodyElement
+    tiles.forEach( tile => body.appendChild(tile))
+}
