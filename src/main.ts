34959706bc0d51de64b2c202123f2b20bdc78f94
aymeric <rambaudaymeric@gmail.com>
2023-08-16 15:06:53 +0200

    bouton start

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('tata')
+const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
+
+let cpt = 0;
+
+btnStart.addEventListener("click", () => {
+    init();
+});
+
+function init(){
+    console.log('start')
+    btnStart.remove();
+}
+
